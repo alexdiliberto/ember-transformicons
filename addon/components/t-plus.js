e01e3c300da2b957130b01d7b050898724a4aaa6
Alex DiLiberto <alex@alexdiliberto.com>
2015-02-13 00:59:46 -0500

    Improve {{t-plus}} handling

--- addon/components/t-plus.js
+++ addon/components/t-plus.js
@@ -1,21 +1,28 @@
 import Ember from 'ember';
 
-var get       = Ember.get;
-var computed  = Ember.computed;
-var Component = Ember.Component;
-
-var defaultType = 'minus';
-var plusTypeTable = {
+var get                = Ember.get;
+var computed           = Ember.computed;
+var alias              = computed.alias;
+var Component          = Ember.Component;
+var transformClass     = 'tcon-transform';
+var defaultAnimation   = 'minus';
+var animationTypeTable = Ember.Object.create({
   'minus': 'tcon-plus tcon-plus--minus',
   'check': 'tcon-plus tcon-plus--check',
   'minus-fold': 'tcon-plus--minusfold',
   'circle': 'tcon-plus--circle'
-};
+});
 
 /**
   Transformicon Add/Remove component.
 
-  Available types:
+  PUBLIC - Optional parameters:
+    * `animation` string - Set the menu animation type.
+    * `a` string - Shorthand alias for `animation`.
+    * `is-added` boolean - Set initial open menu state.
+    * `action` string - The name of your controller/route action to handle an icon click. Returned with 1 parameter `isAdded`, which is a boolean type indication wether the current state is pending add or pending remove.
+
+  Available `animation` types:
     * minus
     * check
     * minus-fold
@@ -24,8 +31,12 @@ var plusTypeTable = {
   Examples:
 
     ```hbs
+      {{! These are functionally equivalent}}
+
       {{t-plus}}
-      {{t-plus type='check'}}
+      {{t-plus a='minus'}}
+      {{t-plus animation='minus'}}
+      {{t-plus is-added=false animation='minus'}}
     ```
 
   @class TPlusComponent
@@ -39,8 +50,24 @@ export default Component.extend({
   'aria-label': 'add item',
 
   classNames: ['tcon'],
-  classNameBindings: ['plusType'],
-  plusType: computed('type', function() {
-    return plusTypeTable[get(this, 'type')] || plusTypeTable[defaultType];
-  })
+  classNameBindings: ['animationType', 'isOpen'],
+  animationType: computed('animation', function() {
+    var anim = get(this, 'animation');
+    return animationTypeTable.get(anim) || animationTypeTable.get(defaultAnimation);
+  }),
+  isOpen: computed('is-added', function() {
+    return get(this, 'is-added') ? transformClass : false;
+  }),
+
+  click: function() {
+    this.toggleProperty('is-added');
+    this.sendAction('action', get(this, 'is-added'));
+  },
+
+  /**
+    PUBLIC
+   */
+  animation: defaultAnimation,
+  a: alias('animation'),
+  'is-added': false
 });
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   isMenuOpen: false,
   isGridOpen: false,
+  isAdded: false,
   actions: {
     updateMenuControllerProperty: function(isOpen) {
       this.set('isMenuOpen', isOpen);
     },
     updateGridControllerProperty: function(isOpen) {
       this.set('isGridOpen', isOpen);
+    },
+    updateAddRemoveControllerProperty: function(isAdded) {
+      this.set('isAdded', isAdded);
     }
   }
 });
