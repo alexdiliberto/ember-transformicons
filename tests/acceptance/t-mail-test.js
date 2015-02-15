3bbdbc17e438d0038dda7fa4fb61e693396ce994
Alex DiLiberto <alex@alexdiliberto.com>
2015-02-15 01:25:20 -0500

    Adding basic acceptance tests for each component

---
+++ tests/acceptance/t-mail-test.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+import { run } from '../helpers/common';
+
+var application;
+
+module('Acceptance: TMail', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    run(application, 'destroy');
+  }
+});
+
+test('Toggle the default {{t-mail}} component', function(assert) {
+  assert.expect(2);
+
+  visit('/');
+
+  var $defaultMail = find('#mail-default');
+
+  assert.equal($defaultMail.hasClass('tcon-transform'), false);
+  $defaultMail.click();
+  andThen(function() {
+    assert.equal($defaultMail.hasClass('tcon-transform'), true);
+  });
+});
+
+test('Verify default {{t-mail}} component has correct template', function(assert) {
+  assert.expect(2);
+
+  visit('/');
+
+  var $defaultMail = find('#mail-default');
+
+  assert.equal($defaultMail.find('span.tcon-mail--envelope__flap').attr('aria-hidden'), 'true');
+  assert.equal($defaultMail.find('span.tcon-visuallyhidden').text(), 'open mailbox');
+});
--- tests/acceptance/t-menu-test.js
+++ tests/acceptance/t-menu-test.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
+import { run } from '../helpers/common';
 
 var application;
 
@@ -10,11 +11,13 @@ module('Acceptance: TMenu', {
   },
 
   afterEach: function() {
-    Ember.run(application, 'destroy');
+    run(application, 'destroy');
   }
 });
 
 test('Toggle the default {{t-menu}} component', function(assert) {
+  assert.expect(2);
+
   visit('/');
 
   var $defaultMenu = find('#menu-default');
@@ -24,4 +27,15 @@ test('Toggle the default {{t-menu}} component', function(assert) {
   andThen(function() {
     assert.equal($defaultMenu.hasClass('tcon-transform'), true);
   });
+});
+
+test('Verify default {{t-menu}} component has correct template', function(assert) {
+  assert.expect(2);
+
+  visit('/');
+
+  var $defaultMenu = find('#menu-default');
+
+  assert.equal($defaultMenu.find('span.tcon-menu__lines').attr('aria-hidden'), 'true');
+  assert.equal($defaultMenu.find('span.tcon-visuallyhidden').text(), 'toggle menu');
 });
