6470a946826d30673ee805862d839810f6438bbb
Alex DiLiberto <alex@alexdiliberto.com>
2015-04-13 18:19:23 -0400

    broccoli-sass -> ember-cli-sass

--- Brocfile.js
+++ Brocfile.js
@@ -3,11 +3,7 @@
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-var app = new EmberAddon({
-  minifyCSS: {
-    enabled: true
-  }
-});
+var app = new EmberAddon({});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
