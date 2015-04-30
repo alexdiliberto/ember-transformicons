7d8b1bfcd696ef87aec39a4ef219893a3496189b
Alex DiLiberto <alex@alexdiliberto.com>
2015-04-30 19:48:29 -0400

    Adding more remove icons

--- addon/components/t-remove.js
+++ addon/components/t-remove.js
@@ -3,15 +3,33 @@ import BaseTransformicon from './base-transformicon';
 
 const {
   get,
-  computed
+  computed,
+  computed: { alias }
 } = Ember;
+const defaultAnimation   = 'check';
+const animationTypeTable = Ember.Object.create({
+  'check':         'tcon-remove--check',
+  'chevron-left':  'tcon-remove--chevron-left',
+  'chevron-right': 'tcon-remove--chevron-right',
+  'chevron-down':  'tcon-remove--chevron-down',
+  'chevron-up':    'tcon-remove--chevron-up'
+});
 
 /**
   Transformicon Remove component.
 
   PUBLIC - Optional parameters:
+    * `animation` string - Set the menu animation type.
+    * `a` string - Shorthand alias for `animation`.
     * `is-removed` boolean - Set initial open removed state.
     * `action` string - The name of your controller/route action to handle an icon click. Returned with 1 parameter `isRemoved`, which is a boolean type indicating if the current state is pending remove.
+
+  Available `animation` types:
+    * check
+    * chevron-left
+    * chevron-right
+    * chevron-down
+    * chevron-up
 
   Examples:
 
@@ -19,7 +37,9 @@ const {
       {{! These are functionally equivalent}}
 
       {{t-remove}}
-      {{t-remove is-removed=false}}
+      {{t-remove a='check'}}
+      {{t-remove animation='check'}}
+      {{t-remove is-removed=false animation='check'}}
     ```
 
   @class TRemoveComponent
@@ -28,8 +48,18 @@ const {
 export default BaseTransformicon.extend({
   'aria-label': 'remove item',
 
-  classNames: ['tcon-remove', 'tcon-remove--check'],
-  classNameBindings: ['isRemoved'],
+  classNames: ['tcon-remove'],
+  classNameBindings: ['animationType', 'isRemoved'],
+  /**
+    Get the CSS classname corresponding to the component's current animation type.
+
+    @property animationType
+    @type String
+  */
+  animationType: computed('animation', function() {
+    const anim = get(this, 'animation');
+    return animationTypeTable.get(anim) || animationTypeTable.get(defaultAnimation);
+  }),
   /**
     Get the classname representing the `remove` toggled state for the remove icon. This classname is stored in the `BaseTransformiconComponent`.
 
@@ -45,5 +75,7 @@ export default BaseTransformicon.extend({
   /**
     PUBLIC COMPONENT PROPERTIES
    */
+  animation: defaultAnimation,
+  a: alias('animation'),
   'is-removed': false
 });
