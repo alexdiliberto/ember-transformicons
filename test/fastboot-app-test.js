0abbd6e3fccfac32de840f68c8b03669178aa264
Alex DiLiberto <alex@alexdiliberto.com>
2017-08-20 10:55:20 -0400

    chore(npm): bump deps

--- test/fastboot-app-test.js
+++ test/fastboot-app-test.js
@@ -9,7 +9,7 @@ const ui = new(require('console-ui'))({
   outputStream: process.stdout
 });
 
-const FASTBOOT_VERSION = '^1.0.2';
+const FASTBOOT_VERSION = '^1.0.5';
 
 describe('Acceptance | consuming fastboot app', function() {
   this.timeout(300000);
