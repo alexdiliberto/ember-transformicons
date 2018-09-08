950d297fe0e950d547f4e5bddd31b7718c5c52d2
Alex DiLiberto <alex@alexdiliberto.com>
2018-09-08 14:14:51 -0400

    feat(typescript) initial typescript setup

---
+++ types/dummy/lodash.defaultto.d.ts
@@ -0,0 +1,4 @@
+declare module 'lodash.defaultto' {
+  // SEE: https://github.com/lodash/lodash/blob/master/defaultTo.js
+  export default function _defaultTo(value: any, defaultValue: any): any;
+}
