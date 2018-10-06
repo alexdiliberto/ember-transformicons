173487b381cef0ecf3b19d1c35a85456a7562589
Alex DiLiberto <alex@alexdiliberto.com>
2018-10-06 14:35:14 -0400

    chore(yarn.lock): update deps

---
+++ tests/dummy/config/dependency-lint.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  allowedVersions: {
+    // Fails unless every instance of ember-assign-polyfill is either 2.4.0 or 2.5.0
+    'ember-assign-polyfill': '2.4.0 || 2.5.0'
+  }
+};
