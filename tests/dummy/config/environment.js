30aa9e0ce9fab68f4fd0d00bd9244a42fda35bf1
Alex DiLiberto <alex@alexdiliberto.com>
2019-02-23 12:54:21 -0500

    build(npm): update fastboot, fastboot-addon-tests and ember-fetch

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -20,6 +20,13 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    fastboot: {
+      hostWhitelist: [
+        'alexdiliberto.com',
+        /^localhost:\d+$/
+      ]
     }
   };
 
