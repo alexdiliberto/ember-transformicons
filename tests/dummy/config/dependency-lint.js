ab144090cc3c89e625a1d48f6d193579b31357ad
Alex DiLiberto <alex@alexdiliberto.com>
2019-08-02 16:56:04 -0400

    test(lint): remove `ember-assign-polyfill` from dependency-lint allowed versions list

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -4,7 +4,6 @@
 module.exports = {
   allowedVersions: {
     // NOTE: Necessary until we can upgrade all dependencies in the tree relying on these packages
-    'ember-assign-polyfill': '^2.4.0',
     'ember-cli-clipboard': '*'
   }
 };
