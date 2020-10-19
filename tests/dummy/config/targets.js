8f80d8b384b90e7a6f71d2de5ffd68d74b212c10
Alex DiLiberto <alex@alexdiliberto.com>
2020-10-19 14:05:28 -0400

    bump cli, min emberjs version supported 3.16

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -7,13 +7,6 @@ const browsers = [
   'last 1 Edge versions'
 ];
 
-// const isCI = !!process.env.CI;
-// const isProduction = process.env.EMBER_ENV === 'production';
-//
-// if (isCI || isProduction) {
-//   browsers.push('ie 11');
-// }
-
 module.exports = {
   browsers
 };
