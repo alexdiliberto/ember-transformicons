c0fde7f1d262537b95614f5ba7476e9aa36294f6
Alex DiLiberto <alex@alexdiliberto.com>
2020-11-14 22:12:31 -0500

    updates

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -4,6 +4,7 @@
 module.exports = {
   allowedVersions: {
     // NOTE: Necessary until we can upgrade all dependencies in the tree relying on these packages
-    'ember-test-waiters': '*'
+    'ember-test-waiters': '*',
+    'ember-truth-helpers': '*'
   }
 };
