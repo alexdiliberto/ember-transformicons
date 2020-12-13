46158bd1d783eabc101f9ffcafad7be3132ceb42
Alex DiLiberto <alex@alexdiliberto.com>
2020-12-12 19:02:03 -0500

    another type fix

--- types/@ember/component/template-only.d.ts
+++ types/@ember/component/template-only.d.ts
@@ -1,6 +1,4 @@
 declare module '@ember/component/template-only' {
   // TODO: remove when this actually exists
-  // tslint:disable-next-line:no-empty-interface
-  export interface TemplateOnlyComponent extends _TemplateOnlyComponent {}
-  export default function templateOnly(moduleName?: string): TemplateOnlyComponent;
+  export default function templateOnly(moduleName?: string): any;
 }
