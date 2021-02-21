25d2bdd1f985c1be78c16d0588084d4a489c2c97
Alex DiLiberto <alex@alexdiliberto.com>
2021-02-20 23:10:39 -0500

    @typescript-eslint/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     '@alexdiliberto',
     'plugin:ember/recommended',
     'plugin:qunit/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint'
   ],
