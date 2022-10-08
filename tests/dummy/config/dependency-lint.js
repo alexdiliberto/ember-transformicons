23639d284c6a4dc7c129a7a4bd3b7a840cf14147
Alex DiLiberto <alex@alexdiliberto.com>
2022-10-08 15:26:00 -0400

    dependency lint

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -5,6 +5,8 @@ module.exports = {
   allowedVersions: {
     // NOTE: Necessary until we can upgrade all dependencies in the tree relying on these packages
     '@ember/render-modifiers': '*',
+    '@embroider/util': '*',
+    '@glimmer/component': '*',
     'ember-test-waiters': '*',
     'ember-truth-helpers': '*',
   },
