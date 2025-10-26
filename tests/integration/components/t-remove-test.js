80a61b9f6765933396a6861fe44645abff66e157
Alex DiLiberto <alex@alexdiliberto.com>
2025-10-25 22:04:00 -0400

    percy updates

--- tests/integration/components/t-remove-test.js
+++ tests/integration/components/t-remove-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { percySnapshot } from 'ember-percy';
+import percySnapshot from '@percy/ember';
 
 module('Integration | Component | t remove', function (hooks) {
   setupRenderingTest(hooks);
